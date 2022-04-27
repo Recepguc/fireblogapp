ef8c369fe60122240d0ba11653621ccd1d736695
Recepguc <recep.gucc@gmail.com>
2022-04-28 00:22:51 +0300

    private router

--- src/app-router/Approuter.js
+++ src/app-router/Approuter.js
@@ -1,28 +1,32 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import React from 'react'
-import Dashboard from '../pages/Dashboard'
-import Login from '../pages/Login'
-import Register from '../pages/Register'
-import Navbar from '../compenents/Navbar'
-import NewBlog from '../pages/NewBlog'
+import React from "react";
+import Dashboard from "../pages/Dashboard";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Navbar from "../compenents/Navbar";
+import NewBlog from "../pages/NewBlog";
 
 import Profile from "../pages/Profile";
+import PrivateRouter from "./PrivateRouter";
+import Details from "../pages/Details";
 
 const AppRouter = () => {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/newblog" element={<NewBlog/>} />
-                
-                
-            </Routes>
-        </Router>
-    );
-}
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route element={<PrivateRouter />}>
+          <Route path="/details/" element={<Details />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/newblog" element={<NewBlog />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
 
 export default AppRouter;
--- src/compenents/BlogCard.js
+++ src/compenents/BlogCard.js
@@ -11,6 +11,7 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import { useContext } from "react";
 import { AuthContext } from "../contexts/AutContext";
+import { useNavigate } from "react-router-dom";
 
 export default function BlogCard({ item, index }) {
   const { currentUser } = useContext(AuthContext);
@@ -18,6 +19,7 @@ export default function BlogCard({ item, index }) {
   const [likeNumber, setLikeNumber] = useState(0);
   const [likeColor, setLikeColor] = useState();
   const [click, setClick] = useState(true);
+  const navigate = useNavigate();
 
   ///like make red and +1 function
   const handleLike = () => {
@@ -31,51 +33,56 @@ export default function BlogCard({ item, index }) {
       setClick(!click);
     }
   };
+  const handleClick = () => {
+    navigate("/details", { state: { item } });
+  };
 
   return (
     <Card sx={{ width: 300, height: 500 }}>
-      <CardMedia
-        component="img"
-        height="100"
-        image={item.imageURL}
-        alt="Paella dish"
-        objectfit="contain"
-      />
-      <CardContent>
-        <Typography
-          variant="body2"
-          color="text.secondary"
-          sx={{
-            display: "block",
-            backgroundColor: "#EFEEFE",
-            padding: "0.5rem",
-            fontFamily: "Girassol",
-          }}
-        >
-          <div
-            style={{
-              paddingTop: "1rem",
-              textAlign: "center",
-              color: "#046582",
+      <div onClick={handleClick}>
+        <CardMedia
+          component="img"
+          height="100"
+          image={item.imageURL}
+          alt="Paella dish"
+          objectfit="contain"
+        />
+        <CardContent>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={{
+              display: "block",
+              backgroundColor: "#EFEEFE",
+              padding: "0.5rem",
+              fontFamily: "Girassol",
             }}
           >
-            <h3>{item.title}</h3>
-            <h6 style={{ color: "grey" }}>{item.date}</h6>
-          </div>
-          {item.content}
-        </Typography>
+            <div
+              style={{
+                paddingTop: "1rem",
+                textAlign: "center",
+                color: "#046582",
+              }}
+            >
+              <h3>{item.title}</h3>
+              <h6 style={{ color: "grey" }}>{item.date}</h6>
+            </div>
+            {item.content}
+          </Typography>
 
-        <Typography
-          variant="body2"
-          color="text.secondary"
-          sx={{ color: "black", textAlign: "start", mt: 2 }}
-        >
-          <IconButton sx={{ color: "black", p: 0 }}>
-            <AccountCircleIcon fontSize="small" />
-          </IconButton>
-          {item.author}
-        </Typography>
-      </CardContent>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={{ color: "black", textAlign: "start", mt: 2 }}
+          >
+            <IconButton sx={{ color: "black", p: 0 }}>
+              <AccountCircleIcon fontSize="small" />
+            </IconButton>
+            {item.author}
+          </Typography>
+        </CardContent>
+      </div>
       <CardActions disableSpacing>
         <IconButton
           onClick={() => {
