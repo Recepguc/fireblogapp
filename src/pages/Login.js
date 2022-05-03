9b7fb91ee7e7f792a3a25e22c2ba19d31f65ab79
Recepguc <recep.gucc@gmail.com>
2022-05-03 13:23:24 +0300

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,37 +1,37 @@
-import * as React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import blokicon from "../assets/blok.png";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import google from "../assets/google.png";
-import "./LoginRegister.css"
+import "./LoginRegister.css";
 import { signIn, signUpProvider } from "../helpers/Firebase";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { AuthContext } from "../contexts/AutContext";
+import ToastNotify from "../helpers/ToastNotify";
 
 export default function SimpleContainer() {
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
-  const navigate =useNavigate();
-  const handleSubmit=(e)=>{
-    e.preventDefault()
-    signIn(email,password,navigate)
-    console.log(email,password);
-  }
-  const handleProviderLogin=()=>{
-    signUpProvider(navigate)
-  }
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const { currentUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signIn(email, password, navigate);
+    ToastNotify(`${currentUser} Login Successfully`);
+    console.log(email, password);
+  };
+  const handleProviderLogin = () => {
+    signUpProvider(navigate);
+  };
   return (
     <div className="loginContainer">
-      <React.Fragment>
-        <CssBaseline />
-        <Container maxWidth="sm">
+      <Container maxWidth="sm">
         <form onSubmit={handleSubmit}>
           <Box className="loginBox">
             <img src={blokicon} alt="blokicon" className="blockIcon" />
-            <h2  >── LOGIN ──</h2>
+            <h2>── LOGIN ──</h2>
             <div className="loginTextField">
               <TextField
                 id="outlined-basic"
@@ -50,20 +50,20 @@ export default function SimpleContainer() {
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
-            <div className="loginButtons" >
-              <Button variant="contained" type="submit" >LOGIN</Button>
+            <div className="loginButtons">
+              <Button variant="contained" type="submit">
+                LOGIN
+              </Button>
               <Button variant="text" onClick={handleProviderLogin}>
                 With
                 <span>
-                  
                   <img src={google} alt="google" className="loginLogo" />
                 </span>
               </Button>
             </div>
           </Box>
-          </form>
-        </Container>
-      </React.Fragment>
+        </form>
+      </Container>
     </div>
   );
 }
