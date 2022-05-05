b61bd93fa3562e9547b09f92fd2b5823b473133a
Recepguc <recep.gucc@gmail.com>
2022-05-05 22:12:36 +0300

    upd

--- src/helpers/Firebase.js
+++ src/helpers/Firebase.js
@@ -11,16 +11,6 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
-// const firebaseConfig = {
-//     // apiKey: "AIzaSyA70KQVNpdnPdvuCzt1PZ4V-ueT3Ho6dSU",
-//     // authDomain: "blog-page-3d264.firebaseapp.com",
-//     // projectId: "blog-page-3d264",
-//     // storageBucket: "blog-page-3d264.appspot.com",
-//     // messagingSenderId: "117452135644",
-//     // appId: "1:117452135644:web:c27e538f06885b8b7ba819"
-//   };
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
